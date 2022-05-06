105cb3d5dea19c2c4008093fc5e17836c0438dcb
DavidGentili <DavidGenntili@gmail.com>
2022-05-06 15:28:49 -0300

    add schema program

--- src/models/Program.js
+++ src/models/Program.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const ProgramSchema = new Schema({
     name: { type: String, require: true },
-    startHour: { type: Date, require: true },
-    FinishHour: { type: Date, require: true },
-    days : { type: Array, require: true },
+    startHour: { type: String, require: true },
+    finishHour: { type: String, require: true },
+    days : { type: [Boolean], require: true },
     highlighted : { type: Boolean, required: true },
     urlImage: { type: String },
 }, { versionKey: false });
