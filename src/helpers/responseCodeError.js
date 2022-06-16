622a9e9df61b3c893690f87d658da434c6715be7
DavidGentili <DavidGenntili@gmail.com>
2022-06-16 10:54:41 -0300

    console log in response error

--- src/helpers/responseCodeError.js
+++ src/helpers/responseCodeError.js
@@ -1,4 +1,5 @@
 const responseCodeError = (e, res) => {
+    console.log(e);
     const {code = 500, response = {message: 'Internal Server Error'}} = e;
     res.statusCode = code;
     res.json(response)
