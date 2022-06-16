3d1e01dc36443bcd763133649b0fdf2d49f80ca9
DavidGentili <DavidGenntili@gmail.com>
2022-06-16 15:52:04 -0300

    StorageFile controller created

--- src/controllers/storageFile.controller.js
+++ src/controllers/storageFile.controller.js
@@ -1,7 +1,40 @@
 const StorageFile = require('../models/StorageFile');
+const { formatObjectResponse } = require('../helpers/formatData');
 
 
 const getFiles = async (name) => {
-    const searchParemeters = name ? { name } : {};
-    const files = await StorageFile.find(searchParemeters).lean();
+    const files = await StorageFile.find().lean();
+    return (Array.isArray(files)) ? files.map(file => formatObjectResponse(file)) : formatObjectResponse(files);
 }
+
+const getFileByName = async ( name ) => {
+    const file = await StorageFile.findOne({ name }).lean();
+    return formatObjectResponse(file);
+}
+
+const createFile = async (name, data) => {
+    const currentFile = await getFileByName(name);
+    if(createFile) 
+        throw { code: 500, response: { message : 'Error al crear el archivo'}};
+    const newFile = new StorageFile({ name, data });
+    await newFile.save();
+    return { message: 'Archivo creado con exito' };
+}
+
+const deleteFileById = async ( id ) => {
+    if(!id && typeof(id) !== 'string' && id.length !== 24)
+        throw { code: 500, response: { message : 'Error al eliminar el archivo'}};
+    await StorageFile.findByIdAndDelete(id);
+    return { message: 'Archivo eliminado con exito' };
+}
+
+const deleteFileByName = async ( name ) => {
+    const currentFile = getFileByName(name);
+    if(!currentFile)
+        throw { code: 500, response: { message : 'Error al eliminar el archivo'}};
+    const { id } = currentFile;
+    await StorageFile.findByIdAndDelete(id);
+    return { message: 'Archivo eliminado con exito' };
+}
+
+module.exports = { getFiles, getFileByName, createFile, deleteFileById, deleteFileByName}
--- src/middlewares/public.middlewares.js
+++ src/middlewares/public.middlewares.js
@@ -1,11 +1,11 @@
-const StorageFile = require('../models/StorageFile');
 const { createTmpImageFile, existFile } = require('../helpers/storage');
+const { getFileByName } = require('../controllers/storageFile.controller');
 
 const checkIfExistFile = async (req, res, next) => {
     try{
         const fileName = req.path.split('/').pop();
         if(!existFile(fileName)){
-            const tempImage = await StorageFile.findOne({name: fileName});
+            const tempImage = getFileByName(fileName);
             if(tempImage){
                 const { name, data } = tempImage;
                 createTmpImageFile(name, data);
