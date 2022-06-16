368c74f1f6d428819ac0c24be60befc252531120
DavidGentili <DavidGenntili@gmail.com>
2022-06-16 11:28:29 -0300

    .

--- src/helpers/storage.js
+++ src/helpers/storage.js
@@ -4,12 +4,11 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, '/public/');
+      cb(null, './public/');
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
       const extension = file.originalname.split('.').pop();
-      console.log(file.fieldname);
       cb(null, `${file.fieldname}-${uniqueSuffix}.${extension}`);
     }
   })
