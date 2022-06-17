b659f3c8eef49ef443bba296d702023fa9c7a961
DavidGentili <DavidGenntili@gmail.com>
2022-06-17 15:59:23 -0300

    add remove file in remove ad

--- src/controllers/publicity.controller.js
+++ src/controllers/publicity.controller.js
@@ -3,9 +3,9 @@ const { formatObjectResponse, getFormatParameters } = require('../helpers/format
 const { createFile } = require ('./storageFile.controller');
 const { checkNewPublicityData } = require('../helpers/checkData');
 const { host } = require('../config');
-const { getNewFileName } = require('../helpers/storage');
+const { getNewFileName, deleteFileByName } = require('../helpers/storage');
 
-
+//Crea un nuevo anuncio, asignando el nombre al archivo, y almacenandolo.
 const createAd = async ( {name, altText, link, type, creatorName, creatorId, imageFile} ) => {
     const check = checkNewPublicityData(name, type);
     if(check !== true)
@@ -19,6 +19,8 @@ const createAd = async ( {name, altText, link, type, creatorName, creatorId, ima
     return { message : 'La publicidad ha sido creada exitosamente'}
 }
 
+//Se encarga de actualizar la data de un anuncio, lo unico que no se puede modificar es la imagen, debido a que se entiende que cada imagen
+//es un anuncio distinto.
 const updateAd = async (data) => {
     const { name, altText, link, type, adId } = data;
     if(!adId || adId.length !== 24 )
@@ -28,15 +30,16 @@ const updateAd = async (data) => {
     return { message: 'ad updated successfully' }
 }
 
+//Se encarga de eliminar el anuncio 
 const deleteAd = async (adId) => {
     if(!adId || adId.length !== 24)
         throw {code: 400, response: { message: 'wrong parameters'}};
     const ad = await Publicity.findById(adId).lean();
     if(!ad)
         throw {code: 400, response: { message: 'wrong parameters'}};
-    const url = ad.urlImage;
+    const nameFile = ad.urlImage.split('/').pop();
+    await deleteFileByName(nameFile);
     await Publicity.findByIdAndDelete(adId);
-    removeFile(url.replace(host,'.'));
     return { message: 'the ad was removed sucessfull'}
 }
 
