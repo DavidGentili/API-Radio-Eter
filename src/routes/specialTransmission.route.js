ad43366505fccd2e1a54b8cb69a0eb6bca0e5bb1
DavidGentili <DavidGenntili@gmail.com>
2022-07-01 14:11:48 -0300

    .

--- src/routes/specialTransmission.route.js
+++ src/routes/specialTransmission.route.js
@@ -40,7 +40,7 @@ router.get('/specialtransmission', isAuthenticated, correctSecurityLevel, async 
 
 })
 
-router.put('/specialTransmission', isAuthenticated, correctSecurityLevel, (req, res) => {
+router.put('/specialtransmission', isAuthenticated, correctSecurityLevel, (req, res) => {
     const { name, startTransmission, finishTransmission, active, transmissionId } = req.body;
     const data = {
         name,
