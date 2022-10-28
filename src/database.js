2cc180e8b06202f9290772c4c1c4daba30d94079
DavidGentili <DavidGenntili@gmail.com>
2022-10-28 16:27:25 -0300

    add console.log

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const {mongodbURL} = require('./config');
 
+console.log(mongodbURL)
+
 mongoose.connect(mongodbURL)
 .then( () => {
     if(mongodbURL && typeof(mongodbURL) === 'string')
