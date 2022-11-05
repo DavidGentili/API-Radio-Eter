80a9140f0045f1f3add39032846f032073b58b16
DavidGentili <DavidGenntili@gmail.com>
2022-11-05 01:46:45 -0300

    generic interactive with Models mongoose

---
+++ src/controllers/element.controller.js
@@ -0,0 +1,102 @@
+const { formatObjectResponse, getFormatParameters } = require('../helpers/formatData');
+const { checkId } = require('../helpers/checkData');
+const { default: mongoose } = require('mongoose');
+
+/**
+ * Se encarga de crear el elemento dentro del modelo ingresado
+ * @param {*} dataElement : data del nuevo elemento
+ * @param {mongoose.Model} Model : modelo del elemento que se desea crear
+ * @returns {message : String} un objeto con el atributo message con el mensaje de exito
+ */
+const createElement = async ( dataElement, Model ) => {
+    try{
+        const newElement = new Model({dataElement});
+        await newElement.save();
+        return { message : 'el elemento se ha creado exitosamente'};
+
+    } catch(e){
+        throw { code : 500, response : { message : 'Error al crear el elemento'}}
+    }
+}
+
+/**
+ * Se encarga de actualizar un elemento del modelo ingresado
+ * pre: La data debe estar formateada adecuadamente
+ * @param {*} dataElement : data el elemento
+ * @param {String} elementId : id del elemento
+ * @param {mongoose.Model} Model : Modelo del elemento a modificar
+ * @returns : mensaje de exito de la operacion
+ */
+const updateElement = async (elementData, elementId, Model) => {
+    if(!elementId || !checkId(elementId) )
+        throw {code: 400, response: { message: 'Parametros incorrectos'}};
+    try{
+        await Model.findByIdAndUpdate(elementId, elementData);
+        return { message : 'El elemento fue actualizado con exito' }
+    } catch(e){
+        throw { code : 500, response : { message : 'Error al actualizar el elemento'}}
+    }
+}
+
+/**
+ * Se encarga de eliminar un elemento con un id y un modelo correspondiente
+ * @param {String} elementId : id del elemento a eliminar
+ * @param {mongoose.Model} Model : Modelo del elemento
+ * @returns : mensaje de exito de la operacion
+ */
+const deleteElement = async (elementId, Model) => {
+    if(!elementId || !checkId(elementId))
+        throw {code: 400, response: { message: 'Parametros incorrectos'}};
+    const element = await Model.findById(elementId);
+    if(!element)
+        throw {code: 400, response: { message: 'numero de id incorrecto'}};       
+    try{
+        await Model.findByIdAndDelete(elementId);
+        return { message: 'El aviso fue removido con exito'}
+    }catch(e){
+        throw { code : 500, response : { message : 'Error al eliminar el elemento'}}
+    }
+}
+
+/**
+ * retorna un arreglo con los elementos del modelo que cumplan con la query 
+ * pre: el criterio de busqueda debe estar correctamente formateado para evitar undefined o resultados incorrectos
+ * @param {*} query : criterio de busqueda
+ * @param {mongoose.Model} Model : Modelo en el cual buscar
+ * @returns arreglo con los elementos correspondientes al criterio de busqueda
+ */
+const getElements = async (query, Model) => {
+    try{
+        const elements = await Model.find({query});
+        const formatResponse = Array.isArray(elements) ? elements.map(element => formatObjectResponse(element)) : [formatObjectResponse(elements)];
+        return formatResponse;
+    } catch(e){
+        throw { code : 500, response : { message : 'Error al consultar el elemento'}}
+    }
+
+}
+
+/**
+ * Retorna un elemento dentro del modelo con el id especificado caso contrario excepcion
+ * @param {String} elementId 
+ * @param {mongoose.Model} Model
+ * @return 
+ */
+const getElementById = async (elementId, Model) => {
+    if(!elementId || !checkId(elementId))
+        throw {code: 500, response: { message: 'numero de id incorrecto'}};
+    try{
+     const element = Model.findById(elementId).lean();
+     return formatObjectResponse(element);
+    }catch(e){
+        throw { code : 500, response : { message : 'Error al consultar el elemento'}}
+    }
+}
+
+module.exports = {
+    getElements,
+    getElementById,
+    createElement,
+    deleteElement,
+    updateElement,
+}
---
+++ src/controllers/report.controller.js
@@ -0,0 +1,59 @@
+const Report = require('../models/Report');
+const { getQueryParams, getFormatParameters } = require('../helpers/formatData');
+const { checkNewReportData, checkUpdateReportData } = require('../helpers/checkData/checkReport');
+const { getElements, getElementById, createElement, updateElement, deleteElement } = require('./element.controller');
+
+const getReport = async (reportData) => {
+    const queryParams = getQueryParams(reportData, ['id','title', 'active','creatorId','creatorName']);
+    return await getElements(queryParams, Report);
+}
+
+const getReportById = async (reportId) => {
+    return await getElementById(reportId, Report);
+}
+
+const createReport = async (reportData) => {
+    const check = checkNewReportData(reportData);
+    if(check !== true)
+        throw { code : 400, response : { message : `Se ha ingresado un ${check} incorrecto`}}
+    try{
+        await createElement(reportData, Report);
+        return { message : 'El reporte ha sido creado con exito'}
+    }catch(e){
+        throw { code : 400, response : { message : 'Error al crear el reporte '}};
+    }
+
+}
+
+const deleteReport = async(reportId) => {
+    try{
+        await deleteElement(reportId, Report);
+        return { message : 'El reporte se ha eliminado con exito'};
+    }catch(e){
+        throw { code : 400, response : { message : 'Error al eliminar el reporte '}};
+    }
+
+}
+
+const updateReport = async (reportData) => {
+    const check = checkUpdateReportData(reportData);
+    if(check !== true)
+        throw { code : 400, response : { message : `Se ha ingresado un ${check} incorrecto`}}
+    try{
+        const { reportId } = reportData;
+        const updateData = getFormatParameters(reportData, ['title', 'description', 'content', 'active', 'mainMediaUrl']);
+        await updateElement(updateData, reportId, Report);
+    } catch(e){
+        throw { code : 400, response : { message : 'Error al actualizar el reporte '}};
+    }
+
+}
+
+
+module.export = { 
+    getReport,
+    getReportById,
+    createReport,
+    deleteReport,
+    updateReport,
+}
