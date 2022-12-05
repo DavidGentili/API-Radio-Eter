9f26583a18fdd327978586c43dfb75a2df57aca0
DavidGentili <DavidGenntili@gmail.com>
2022-12-05 12:25:57 -0300

    add date to media files

--- src/models/StorageFile.js
+++ src/models/StorageFile.js
@@ -6,7 +6,14 @@ const StorageFileShema = new Schema({
     name : { type: String, required : true },
     urlName : { type: String, required : true }, 
     data : { type: Buffer, required: true },
+    createdAt : {type: Date, required: true},
 
 }, { versionKey : false });
+
+
+StorageFileShema.pre('save', (next) => {
+    this.createdAt = Date.now();
+    next();
+})
 
 module.exports = mongoose.model('StorageFile', StorageFileShema);
