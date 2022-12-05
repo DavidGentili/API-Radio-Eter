f72a0bb90d7c90a28b2ea894c0831bec24f3c48a
DavidGentili <DavidGenntili@gmail.com>
2022-12-05 14:18:35 -0300

    add timestamps

--- src/models/StorageFile.js
+++ src/models/StorageFile.js
@@ -6,9 +6,10 @@ const StorageFileShema = new Schema({
     name : { type: String, required : true },
     urlName : { type: String, required : true }, 
     data : { type: Buffer, required: true },
-    createdAt : {type: Date, default: Date.now()},
-
-}, { versionKey : false });
+}, { 
+    versionKey : false, 
+    timestamps : true 
+});
 
 
 
